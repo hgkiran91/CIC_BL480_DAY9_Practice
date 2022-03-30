df7b2ea2a9b535a616708356746e07f806c19e17
kiran@DESKTOP-P65JLIU <kiranmcehsn@gmail.com>
2022-03-30 09:40:55 +0530

    Calculating Emp Daily Wage

--- calculateEmpWage.sh
+++ calculateEmpWage.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
 
 PRESENT=1
+WAGE_PER_HOUR=20
+
 isPresent=$((RANDOM%2))
 
 if [ $isPresent -eq $PRESENT ]
 then
 	echo "Employee is present"
+	workingHour=8
 else
 	echo "Employee is absent"
+	workingHour=0
 fi
+
+WAGE=$((WAGE_PER_HOUR*workingHour))
+
+echo "Employee Daily wage: $"$WAGE "USD"
