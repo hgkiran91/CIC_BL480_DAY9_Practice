c4a0361331fcb3fcb04c350b6f87e66489d5e1a1
kiran@DESKTOP-P65JLIU <kiranmcehsn@gmail.com>
2022-04-12 18:35:57 +0530

    Part time Emp wage and Hour

--- calculateEmpWage.sh
+++ calculateEmpWage.sh
@@ -2,13 +2,18 @@
 
 PRESENT=1
 WAGE_PER_HOUR=20
+Part_time=2
 
-isPresent=$((RANDOM%2))
+isPresent=$((RANDOM%3))
 
 if [ $isPresent -eq $PRESENT ]
 then
 	echo "Employee is present"
 	workingHour=8
+elif [ $isPresent -eq $Part_time ]
+then
+	echo "Employee worked part time"
+	workingHour=4
 else
 	echo "Employee is absent"
 	workingHour=0
